4c4c76f839476f44e050509c0b0bbe40aff18cf7
Jesus Lechuga <jesusleg95@outlook.com>
2026-02-09 16:22:07 -0700

    Terminando el formulario de 'Crear Vacante'

--- resources/views/components/⚡crear-vacante.blade.php
+++ resources/views/components/⚡crear-vacante.blade.php
@@ -2,22 +2,61 @@
 
 use App\Models\Salario;
 use App\Models\Categoria;
+use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+
 
 new class extends Component
 {
-    public $salarios, $categorias; 
-    public $titulo, $salario, $categoria, $empresa, $ultimo_dia, $descripcion, $imagen;
+  
+    public $salarios; 
+    public $categorias; 
+    public $titulo; 
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion; 
+    public $imagen;
 
-    protected $rules = [
+    use WithFileUploads;
+
+protected function rules(){
+    return [
         'titulo' => 'required|string',
         'salario' => 'required',
         'categoria' => 'required',
         'empresa' => 'required',
         'ultimo_dia' => 'required',
         'descripcion' => 'required',
-        'imagen' => 'required'
+        'imagen' => 'required|image|max:1024',
     ];
+}
+
+    public function crearVacante(){
+        $datos = $this->validate();
+
+        //Almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes/', '', $imagen);
+        
+        //Crear la vacante
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'],
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $datos['imagen'],
+            'user_id' => Auth::user()->id
+        ]);
+
+        //Crear mensaje
+
+        //Redireccionar al usuario
+    }
 
     public function mount()
     {
@@ -30,6 +69,8 @@ new class extends Component
 <form 
     action=""
     class="md:w-1/2 space-y-5"
+    wire:submit.prevent='crearVacante'
+    enctype="multipart/form-data"
 >
     <div>
         <x-input-label for="titulo" :value="__('Titulo Vacante')" />
@@ -38,9 +79,13 @@ new class extends Component
             class="block mt-1 w-full" 
             type="text" 
             wire:model="titulo" 
-            :value="old('titulo')" required autofocus
+            
             placeholder="Titulo Vacante"
         />
+
+        @error('titulo')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -50,11 +95,15 @@ new class extends Component
             wire:model="salario"
             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
         >
-            <option>-- Seleccione --</option>
+            <option value="">-- Seleccione --</option>
             @foreach ($salarios as $salario)
                 <option value="{{$salario->id}}">{{$salario->salario}}</option>
             @endforeach
         </select>
+
+        @error('salario')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -64,11 +113,15 @@ new class extends Component
             wire:model="categoria"
             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
         >
-            <option>-- Seleccione --</option>
+            <option value="">-- Seleccione --</option>
             @foreach ($categorias as $categoria)
                 <option value="{{$categoria->id}}">{{$categoria->categoria}}</option>
             @endforeach
         </select>
+
+        @error('categoria')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -78,9 +131,13 @@ new class extends Component
             class="block mt-1 w-full" 
             type="text" 
             wire:model="empresa" 
-            :value="old('empresa')" required autofocus
+          
             placeholder="Empresa: Ej. Netflix, Uber, Spotify, etc."
         />
+
+        @error('empresa')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -90,8 +147,11 @@ new class extends Component
             class="block mt-1 w-full" 
             type="date" 
             wire:model="ultimo_dia" 
-            :value="old('ultimo_dia')" required autofocus
+            
         />
+        @error('ultimo_dia')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -103,6 +163,9 @@ new class extends Component
             class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full h-72"   
         >
         </textarea>
+        @error('descripcion')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
@@ -112,7 +175,19 @@ new class extends Component
             class="block mt-1 w-full" 
             type="file" 
             wire:model="imagen" 
+            accept="image/*"
         />
+
+        <div class="my-5 w-80">
+            @if ($imagen)
+                Imagen:
+                <img src="{{$imagen->temporaryUrl()}}">
+            @endif
+        </div>
+
+        @error('imagen')
+           <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <x-primary-button class="w-full justify-center">
